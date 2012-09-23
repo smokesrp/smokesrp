aa19302a7f015cea35e37af95e6f20ce243e4baf
Tyler Stokes <tystokes@umich.edu>
2012-09-23 19:09:48 -0400

    missing then

--- smokesrp/gamemode/cl_init.lua
+++ smokesrp/gamemode/cl_init.lua
@@ -1,7 +1,7 @@
 include("hud.lua")
 
 function GM:HUDShouldDraw(hud) -- What should be draw
-	if(hud == "CHudHealth" || hud == "CHudBattery" || hud == "CHudSuitPower")
+	if(hud == "CHudHealth" || hud == "CHudBattery" || hud == "CHudSuitPower") then
 		return false -- These will not be drawn
 	else
 		return true -- Everything else will be drawn
