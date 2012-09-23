f132ccb758e3ddb09bdef82f05b42d0166bfe179
Tyler Stokes <tystokes@umich.edu>
2012-09-23 19:08:28 -0400

    missing a comma

--- smokesrp/gamemode/jobs.lua
+++ smokesrp/gamemode/jobs.lua
@@ -7,7 +7,7 @@ jobs = {};			--an array that holds all of the jobs
 --defaults this makes a citizen class if you dont pass arguments to the constructor.
 Job = {name = "Citizen", Color( 125, 125, 125, 255 ), loadout = {"weapon_physcannon", "weapon_physgun","gmod_tool"}, cmd = "srp_citizen"};
 function Job:new(name, color, loadout, cmd)
-	local struct = {name = name, color = color loadout = loadout, cmd = cmd, id = (#jobs + 1)};
+	local struct = {name = name, color = color, loadout = loadout, cmd = cmd, id = (#jobs + 1)};
 	setmetatable(struct, {__index = Job});
 	table.insert(jobs, struct);
 	team.SetUp(#jobs, name, color) --creates the team
