444e6957092c21dfe809bd98729ffc6ed230303b
Dylan Stokes <dylan.stokes@comcast.net>
2012-09-23 17:50:06 -0400

    Added Include
    Includes the hud.lua file in cl_init

--- smokesrp/gamemode/cl_init.lua
+++ smokesrp/gamemode/cl_init.lua
@@ -1,3 +1,5 @@
+include("hud.lua")
+
 function GM:HUDShouldDraw(hud) -- What should be draw
 	if(hud == "CHudHealth" || hud == "CHudBattery" || hud == "CHudSuitPower")
 		return false -- These will not be drawn
