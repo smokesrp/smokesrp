6f8f450e0ef507bd727d701660b5e7f017f9a83a
Tyler Stokes <tystokes@umich.edu>
2012-09-23 16:16:11 -0400

    An example of creating a class in lua

---
+++ pseudoclass.lua
@@ -0,0 +1,10 @@
+--A sample of using metatables as a class
+Player = {hp = 100, xp = 0};
+function Player:new(hp, xp)
+	local struct = { hp = hp, xp = xp };
+	setmetatable(struct, {__index = Player});
+	return struct;
+end
+
+john = Player:new();
+print (john.hp,john.xp);
