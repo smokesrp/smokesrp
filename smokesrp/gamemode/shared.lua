f9c61f3613c5fd233b75894f04c8039caf56051e
Dylan Stokes <dylan.stokes@comcast.net>
2012-09-23 20:56:26 -0400

    Deleted For Loop

--- smokesrp/gamemode/shared.lua
+++ smokesrp/gamemode/shared.lua
@@ -5,11 +5,3 @@ GM.Website		= ""
 
 
 DeriveGamemode( "sandbox" )
-
-for index = 1, #jobs do
-	concommand.Add( jobs[index].cmd, function()
-		ply:SetTeam(index)
-		ply:Spawn()
-		ply:PrintMessage( HUD_PRINTTALK, "You have become a "..team.GetName( ply:Team() ).."." )
-	end )
-end
