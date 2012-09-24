1c917baa2dad526ac635d2c863453f4577b26cc0
Dylan Stokes <dylan.stokes@comcast.net>
2012-09-23 23:02:28 -0400

    HUD: Job Name
    Put the Job Name back on the HUD

--- smokesrp/gamemode/cl_init.lua
+++ smokesrp/gamemode/cl_init.lua
@@ -16,7 +16,7 @@ function DrawHud()
 	-- Clients Health --
 	draw.SimpleText( "Health: "..LocalPlayer():Health(), "BudgetLabel", 55, ScrH() - 65, Color( 255, 255, 255, 255 ) )
 	-- Clients Job --
-	--draw.SimpleText( "Job: "..team.GetName( LocalPlayer():Team() ), "BudgetLabel", 55, ScrH() - 50, Color( 255, 255, 255, 255 ) )
+	draw.SimpleText( "Job: "..jobs[LocalPlayer():Team()].name, "BudgetLabel", 55, ScrH() - 50, Color( 255, 255, 255, 255 ) )
 end
 
 function GM:HUDPaint()
