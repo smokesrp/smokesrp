27796d6b3083ed0bc069c6a039fbb931236ea4e5
Tyler Stokes <tystokes@umich.edu>
2012-09-23 21:55:44 -0400

    fixing jobs
    commented police officer fixed a mistake in syntax of default
    constructor

--- smokesrp/gamemode/jobs.lua
+++ smokesrp/gamemode/jobs.lua
@@ -6,7 +6,7 @@ jobs = {};			--an array that holds all of the jobs
 					--at the index corresponding to their ID number
 
 --defaults this makes a citizen class if you dont pass arguments to the constructor.
-Job = {name = "Citizen", Color( 125, 125, 125, 255 ), loadout = {"weapon_physcannon", "weapon_physgun","gmod_tool"}, cmd = "srp_citizen"};
+Job = {name = "Citizen", color = Color( 125, 125, 125, 255 ), loadout = {"weapon_physcannon", "weapon_physgun","gmod_tool"}, cmd = "srp_citizen"};
 function Job:new(name, color, loadout, cmd)
 	local struct = {name = name, color = color, loadout = loadout, cmd = cmd, id = (#jobs + 1)};
 	setmetatable(struct, {__index = Job});
@@ -16,7 +16,7 @@ function Job:new(name, color, loadout, cmd)
 end
 
 Job.new();--creates the citizen job stored in jobs[1]
-Job.new("Police Officer", Color( 125, 125, 125, 255 ), {"gmod_tool"},"srp_police");	--creates an example police job stored in jobs[2]
+--Job.new("Police Officer", Color( 125, 125, 125, 255 ), {"gmod_tool"},"srp_police");	--creates an example police job stored in jobs[2]
 
 net.Receive( "changejob", function() //handles changing jobs
 	local ply = net.ReadEntity()
