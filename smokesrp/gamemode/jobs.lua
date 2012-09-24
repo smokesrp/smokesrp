3eee5dfc2af484e01888d4b4a81636d1dc0d3832
Tyler Stokes <tystokes@umich.edu>
2012-09-23 23:19:37 -0400

    concmd fix
    attempted to fix the concommand inits

--- smokesrp/gamemode/jobs.lua
+++ smokesrp/gamemode/jobs.lua
@@ -3,6 +3,7 @@
 
 jobs = {};			--an array that holds all of the jobs
 					--at the index corresponding to their ID number
+idlist = {};
 
 --defaults this makes a citizen class if you dont pass arguments to the constructor.
 Job = {name = "Citizen", color = Color( 125, 125, 125, 255 ), loadout = {"weapon_physcannon", "weapon_physgun","gmod_tool"}, cmd = "srp_citizen"};
@@ -10,9 +11,10 @@ function Job:new(name, color, loadout, cmd)
 	local struct = {name = name, color = color, loadout = loadout, cmd = cmd, id = (#jobs + 1)};
 	setmetatable(struct, {__index = Job});
 	table.insert(jobs, struct);
+	table.insert(idlist, struct.cmd = struct.id);
 	team.SetUp(#jobs, name, color) --creates the team
-	concommand.Add( struct.cmd, function( ply )//creates console command for switching
-		ply:SetTeam( struct.id )
+	concommand.Add( struct.cmd, function(ply, command)//creates console command for switching
+		ply:SetTeam( idlist[command])
 		ply:Spawn()
 	end )
 	return struct;
