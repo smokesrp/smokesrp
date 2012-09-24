3dcb71832230bf630489966ad5dea2b76d1a521d
Dylan Stokes <dylan.stokes@comcast.net>
2012-09-23 23:25:16 -0400

    Revert

--- smokesrp/gamemode/jobs.lua
+++ smokesrp/gamemode/jobs.lua
@@ -3,7 +3,6 @@
 
 jobs = {};			--an array that holds all of the jobs
 					--at the index corresponding to their ID number
-idlist = {};
 
 --defaults this makes a citizen class if you dont pass arguments to the constructor.
 Job = {name = "Citizen", color = Color( 125, 125, 125, 255 ), loadout = {"weapon_physcannon", "weapon_physgun","gmod_tool"}, cmd = "srp_citizen"};
@@ -11,10 +10,9 @@ function Job:new(name, color, loadout, cmd)
 	local struct = {name = name, color = color, loadout = loadout, cmd = cmd, id = (#jobs + 1)};
 	setmetatable(struct, {__index = Job});
 	table.insert(jobs, struct);
-	table.insert(idlist, struct.cmd = struct.id);
 	team.SetUp(#jobs, name, color) --creates the team
-	concommand.Add( struct.cmd, function(ply)//creates console command for switching
-		ply:SetTeam( struct.id)
+	concommand.Add( struct.cmd, function( ply )//creates console command for switching
+		ply:SetTeam( struct.id )
 		ply:Spawn()
 	end )
 	return struct;
