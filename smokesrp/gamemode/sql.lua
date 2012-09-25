ed04f151b4ba68bc2f2ccae86c37184ce3eba2d8
Dylan Stokes <dylan.stokes@comcast.net>
2012-09-25 16:03:32 -0400

    SQL System
    Basic SQL system was added

---
+++ smokesrp/gamemode/sql.lua
@@ -0,0 +1,68 @@
+function tables_exist()
+	if (!sql.TableExists("smokesrp_money")) then
+		query = "CREATE TABLE smokesrp_money ( unique_id varchar(255), money int )"
+		result = sql.Query(query)
+		if (sql.TableExists("smokesrp_money")) then
+			Msg("Table was created!\n")
+		else
+			Msg("Error 1: Table was not created\n")
+		end	
+	end
+end
+
+function Initialize()
+	tables_exist()
+end
+ 
+function PlayerInitialSpawn( ply )
+ 
+	timer.Create("Steam_id_delay", 1, 1, function()
+		SteamID = ply:SteamID()
+		ply:SetNWString("SteamID", SteamID)
+		timer.Create("SaveStat", 5, 0, function() saveStat( ply ) end)
+		player_exists( ply ) 
+	end)
+ 
+ end
+ 
+hook.Add( "PlayerInitialSpawn", "PlayerInitialSpawn", PlayerInitialSpawn )
+hook.Add( "Initialize", "Initialize", Initialize )
+ 
+ 
+function player_exists( ply )
+	steamID = ply:GetNWString("SteamID")
+	result = sql.Query("SELECT unique_id, money FROM smokesrp_money WHERE unique_id = '"..steamID.."'")
+	if (result) then
+		sql_value_money( ply )
+		Msg("Player in database exists!\n")
+	else
+		new_player( steamID, ply )
+		Msg("Player does not exist.. creating account!\n")
+	end
+end
+ 
+function new_player( SteamID, ply )
+	steamID = SteamID
+	sql.Query( "INSERT INTO smokesrp_money (`unique_id`, `money`)VALUES ('"..steamID.."', '100')" )
+	result = sql.Query( "SELECT unique_id, money FROM smokesrp_money WHERE unique_id = '"..steamID.."'" )
+	if (result) then
+		Msg("Players account was created in the database!\n")
+		sql_value_money( ply )
+	else
+		Msg("Error 2: Players account was not created")
+	end
+end
+
+function sql_value_money ( ply )
+	unique_id = sql.QueryValue("SELECT unique_id FROM smokesrp_money WHERE unique_id = '"..steamID.."'")
+	money = sql.QueryValue("SELECT money FROM smokesrp_money WHERE unique_id = '"..steamID.."'")
+	ply:SetNWString("unique_id", unique_id)
+	ply:SetNWInt("money", money)
+end
+
+function saveStat ( ply )
+	money = ply:GetNWInt("money")
+	unique_id = ply:GetNWString ("SteamID")
+	sql.Query("UPDATE player_info SET money = "..money.." WHERE unique_id = '"..unique_id.."'")
+	Msg("Saving...\n")
+end
