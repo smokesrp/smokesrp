60bfc2ceeb4749881ec02fa88f7fe7317cfc77d7
Tyler Stokes <tystokes@umich.edu>
2012-09-27 16:09:14 -0400

    Jobs is a struct
    Changed jobs from a metatable class to a table.

--- smokesrp/gamemode/jobs.lua
+++ smokesrp/gamemode/jobs.lua
@@ -4,24 +4,15 @@
 jobs = {};				--an array that holds all of the jobs
 					--at the index corresponding to their ID number
 
---defaults this makes a citizen class if you dont pass arguments to the constructor.
-Job = {name = "Citizen", color = Color( 125, 125, 125, 255 ), loadout = {"weapon_physgun", "weapon_physcannon", "gmod_tool"}, cmd = "srp_citizen"};
-function Job:new(name, color, loadout, cmd)
-	local struct = {name = name, color = color, loadout = loadout, cmd = cmd, id = (#jobs + 1)};
-	setmetatable(struct, {__index = Job});
-	table.insert(jobs, struct);
-	team.SetUp(#jobs, name, color); --creates the team
-	return struct;
-end
+table.insert( jobs, {name = "Citizen", color = Color( 125, 125, 125, 255 ), loadout= {"weapon_physgun", "weapon_physcannon", "gmod_tool"}});--creates the citizen job stored in jobs[1]
+table.insert( jobs, {name = "Police Officer", color = Color( 125, 125, 125, 255 ), loadout = {"gmod_tool"}});	--creates an example police job stored in jobs[2]
 
 concommand.Add("srp_job", function(ply, command, args)
-	id = toNumber(args[1]);
-	if(id == nil or id < 1 or id > #job) then
-		return;						--if the argument is nil or less than 1 
+	id = tonumber(args[1]);
+	if(id == nil or id < 1 or id > #jobs) then
+		return;						--if the argument is nil or less than 1
 	end
-	ply::SetTeam(id);					--or greater than the amount of jobs
-	ply::Spawn();
+	ply:SetTeam(id);					--or greater than the amount of jobs
+	ply:Spawn();
+	ply:KillSilent();
 end )
-
-Job.new();--creates the citizen job stored in jobs[1]
-Job.new("Police Officer", Color( 125, 125, 125, 255 ), {"gmod_tool"},"srp_police");	--creates an example police job stored in jobs[2]
