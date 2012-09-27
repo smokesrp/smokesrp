e448d0068ca1a5d91ce5b71e0cd3792f08b48294
Dylan Stokes <dylan.stokes@comcast.net>
2012-09-27 16:18:08 -0400

    Added Pistol to Police
    Added weapon_pistol to the Police Officer Job

--- smokesrp/gamemode/jobs.lua
+++ smokesrp/gamemode/jobs.lua
@@ -5,7 +5,7 @@ jobs = {};				--an array that holds all of the jobs
 					--at the index corresponding to their ID number
 
 table.insert( jobs, {name = "Citizen", color = Color( 125, 125, 125, 255 ), loadout= {"weapon_physgun", "weapon_physcannon", "gmod_tool"}});--creates the citizen job stored in jobs[1]
-table.insert( jobs, {name = "Police Officer", color = Color( 125, 125, 125, 255 ), loadout = {"gmod_tool"}});	--creates an example police job stored in jobs[2]
+table.insert( jobs, {name = "Police Officer", color = Color( 125, 125, 125, 255 ), loadout = {"weapon_physgun", "weapon_physcannon","gmod_tool","weapon_pistol" }});	--creates an example police job stored in jobs[2]
 
 concommand.Add("srp_job", function(ply, command, args)
 	id = tonumber(args[1]);
